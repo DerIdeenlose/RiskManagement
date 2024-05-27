029a1c275975b0d2580ecee59162e4462f838de4
William Shea <william.shea@t-systems.com>
2024-05-27 13:36:28 +0000

    Initial Commit

---
+++ app/risk-magament/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'riskmagament/test/integration/FirstJourney',
+		'riskmagament/test/integration/pages/RisksList',
+		'riskmagament/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('riskmagament') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
